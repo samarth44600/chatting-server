2ed2e9270d12ba952e5cd0e7591b7c9b293be68f
Samarth <71260220+samarth44600@users.noreply.github.com>
2023-05-27 04:41:34 -0700

    Update server.js

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ const port = process.env.PORT;
 const io = socket(httpServer, {
   cors: {
     origin: isProduction
-      ? "https://chat-app-samarth44600.vercel.app"
+      ? ["https://chat-app-samarth44600.vercel.app", "https://chatting-client.vercel.app", "https://chatting-client-samarth44600.vercel.app"]
       : "http://localhost:3000",
   },
 });
